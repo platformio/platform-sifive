5589f19b42d589c39b4d8640f9e15985847bcbc2
valeros <valeros@bigmir.net>
2019-06-26 16:27:48 +0300

    Initial support for SiFive platform

---
+++ builder/main.py
@@ -0,0 +1,112 @@
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from os.path import join
+
+from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Default, DefaultEnvironment)
+
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+board_config = env.BoardConfig()
+
+env.Replace(
+    AR="riscv64-unknown-elf-gcc-ar",
+    AS="riscv64-unknown-elf-as",
+    CC="riscv64-unknown-elf-gcc",
+    GDB="riscv64-unknown-elf-gdb",
+    CXX="riscv64-unknown-elf-g++",
+    OBJCOPY="riscv64-unknown-elf-objcopy",
+    RANLIB="riscv64-unknown-elf-gcc-ranlib",
+    SIZETOOL="riscv64-unknown-elf-size",
+
+    ARFLAGS=["rc"],
+
+    SIZEPRINTCMD='$SIZETOOL -d $SOURCES',
+
+    PROGSUFFIX=".elf"
+)
+
+# Allow user to override via pre:script
+if env.get("PROGNAME", "program") == "program":
+    env.Replace(PROGNAME="firmware")
+
+if not env.get("PIOFRAMEWORK"):
+    env.SConscript("frameworks/_bare.py", exports="env")
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = None
+if "nobuild" in COMMAND_LINE_TARGETS:
+    target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
+else:
+    target_elf = env.BuildProgram()
+
+AlwaysBuild(env.Alias("nobuild", target_elf))
+target_buildprog = env.Alias("buildprog", target_elf, target_elf)
+
+#
+# Target: Print binary size
+#
+
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
+
+#
+# Target: Upload by default .bin file
+#
+
+upload_protocol = env.subst("$UPLOAD_PROTOCOL")
+debug_tools = board_config.get("debug.tools", {})
+upload_actions = []
+
+if upload_protocol in debug_tools:
+    openocd_args = [
+        "-c",
+        "debug_level %d" % (2 if int(ARGUMENTS.get("PIOVERBOSE", 0)) else 1),
+        "-s", platform.get_package_dir("tool-openocd-riscv") or ""
+    ]
+    openocd_args.extend(
+        debug_tools.get(upload_protocol).get("server").get("arguments", []))
+    openocd_args.extend([
+        "-c", "program {$SOURCE} %s verify; shutdown;" %
+        board_config.get("upload").get("flash_start", "")
+    ])
+    env.Replace(
+        UPLOADER="openocd",
+        UPLOADERFLAGS=openocd_args,
+        UPLOADCMD="$UPLOADER $UPLOADERFLAGS")
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+# custom upload tool
+elif upload_protocol == "custom":
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+else:
+    sys.stderr.write("Warning! Unknown upload protocol %s\n" % upload_protocol)
+
+AlwaysBuild(env.Alias("upload", target_elf, upload_actions))
+
+
+#
+# Setup default targets
+#
+
+Default([target_buildprog, target_size])
---
+++ examples/freedom-e-sdk_hello/src/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_hello() {
+    puts("Hello from function!\n");
+}
+
+int main()
+{
+  puts("hello world!\n");
+  
+    while(1) {
+    }
+
+  return 0;
+}
