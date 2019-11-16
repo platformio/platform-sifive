2769c0b031ca163a99a97f387d1196e178a48a58
valeros <valeros@bigmir.net>
2019-11-16 22:34:36 +0200

    Move zephyr project files to a separate folder

--- examples/zephyr-hello-world/zephyr-modules.cmake
+++
@@ -1,3 +0,0 @@
-# Put this in a file with a name like "zephyr-modules.cmake"
-set(ZEPHYR_MODULES $ENV{ZEPHYR_BASE}my-modules/hal_stm32 CACHE STRING "pre-cached modules")
-message($ENV{ZEPHYR_BASE}my-modules/hal_stm32)
